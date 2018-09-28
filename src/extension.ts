cd0d7bdee44087c8dabc47bfcb9aceb1e64419c0
Steven Robbins <ste.robbins@gmail.com>
2018-09-28 15:35:58 +0100

    Removed sample comments

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,8 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as process from 'child_process';
 
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('pico8.runCart', () => {
         if (!vscode.window || !vscode.window.activeTextEditor) {
             vscode.window.showInformationMessage("Error: Unable to find current document to execute.");
@@ -72,6 +64,5 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
 }
